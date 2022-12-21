088da78fd72ad9f9d04e06a758a2f86c4afd368a
Fede Calendino <fede@calendino.com>
2022-12-21 14:08:46 +0100

    Initial release

---
+++ minuteinbox/classes.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from typing import Iterator
+
+from minuteinbox import api
+
+
+class Account:
+    def __init__(self, **data):
+        self.name: str = data["name"]
+        self.address: str = data["address"]
+
+
+class Mail:
+    def __init__(self, **data):
+        self.id: str = data["id"]
+        self.sent_at: str = data["sent_at"]
+        self.subject: str = data["subject"]
+        self.content: str = data["content"]
+        self.sender: Account = Account(**data["sender"])
+
+
+class Inbox:
+    def __init__(self, address: str = None, token: str = None):
+        if not address and not token:
+            address, token = api.inbox()
+
+        self.address: str = address
+        self.token: str = token
+
+    @property
+    def mails(self) -> Iterator[Mail]:
+        for mail in api.refresh(self.address, self.token):
+            yield Mail(**mail)
+
+    def extend_10m(self):
+        api.extend(self.address, self.token, seconds=4200)
+
+    def extend_1h(self):
+        api.extend(self.address, self.token, seconds=3600)
+
+    def extend_1d(self):
+        api.extend(self.address, self.token, seconds=86400)
+
+    def extend_1w(self):
+        api.extend(self.address, self.token, seconds=604800)
+
+    @property
+    def expires_in(self):
+        times = api.times(self.address, self.token)
+
+        if times["expires_at"] is None:
+            raise ValueError(f"{self.address} has expired")
+
+        created_at = datetime.strptime(times["created_at"], "%Y-%m-%d %H:%M:%S")
+        expires_at = datetime.strptime(times["expires_at"], "%Y-%m-%d %H:%M:%S")
+
+        return (expires_at - created_at).seconds
